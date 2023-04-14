595de69dd0d7b52da9dc620588cc705e755a228a
ShaysFrame <akashshaykh@gmail.comm>
2023-04-15 02:03:10 +0800

    staticfiles added modified settings.py with STATICFILES_DIRS variable and so on

--- xuehanyu_django/settings.py
+++ xuehanyu_django/settings.py
@@ -146,7 +146,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'public/') # this is 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # this is 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 
 # Default primary key field type
