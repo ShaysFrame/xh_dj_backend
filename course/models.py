ae16e43367eba2247d4ac22b9df4e2c9d8e5acfc
ShaysFrame <akashshaykh@gmail.comm>
2023-05-06 13:56:44 +0800

    Updated video urls from Youtube to Bilibili

--- course/models.py
+++ course/models.py
@@ -73,7 +73,7 @@ class Lesson(models.Model):
     long_description = models.TextField(blank=True, null=True)
     status = models.CharField(max_length=20, choices=CHOICES_STATUS, default=PUBLISHED)
     lesson_type = models.CharField(max_length=20, choices=CHOICES_LESSON_TYPE, default=ARTICLE)
-    youtube_id = models.CharField(max_length=20, blank=True, null=True)
+    youtube_id = models.CharField(max_length=100, blank=True, null=True)
 
 class Comment(models.Model):
     course = models.ForeignKey(Course, related_name='comments', on_delete=models.CASCADE)
